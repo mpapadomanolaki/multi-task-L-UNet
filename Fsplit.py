d1d4d826f702e93f3369de12bc0254b311aa7a96
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2020-10-15 20:00:46 +0300

    Create Fsplit.py

---
+++ Fsplit.py
@@ -0,0 +1,14 @@
+import numpy as np
+import os
+import glob
+import random
+
+
+FOLDER = glob.glob('/home/mariapap/DATA/SPACENET7/train/*_13*')
+random.shuffle(FOLDER)
+Ftrain = FOLDER[0:40]
+Fval = FOLDER[40:50]
+Ftest = FOLDER[50:60]
+np.save('./Fsplit/Ftrain.npy', Ftrain)
+np.save('./Fsplit/Fval.npy', Fval)
+np.save('./Fsplit/Ftest.npy', Ftest)
