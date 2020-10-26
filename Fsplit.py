9fea43be478b890b46cf73347649d037da138d39
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2020-10-26 18:47:13 +0200

    Update Fsplit.py

--- Fsplit.py
+++ Fsplit.py
@@ -4,7 +4,7 @@ import glob
 import random
 
 
-FOLDER = glob.glob('../train/*_13*')
+FOLDER = glob.glob('/home/mariapap/DATA/SPACENET7/train/*_13*')
 random.shuffle(FOLDER)
 Ftrain = FOLDER[0:40]
 Fval = FOLDER[40:50]
@@ -12,3 +12,8 @@ Ftest = FOLDER[50:60]
 np.save('./Fsplit/Ftrain.npy', Ftrain)
 np.save('./Fsplit/Fval.npy', Fval)
 np.save('./Fsplit/Ftest.npy', Ftest)
+
+
+print(len(Ftrain), 'folders for training')
+print(len(Fval), 'folders for validation')
+print(len(Ftest), 'folders for testing')
