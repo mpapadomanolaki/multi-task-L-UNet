73123bb76523919b0d2427741443c0031843dc47
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2020-10-26 19:24:47 +0200

    Update Fsplit.py

--- Fsplit.py
+++ Fsplit.py
@@ -4,7 +4,7 @@ import glob
 import random
 import shutil
 
-FOLDER = glob.glob('/home/mariapap/DATA/SPACENET7/train/*_13*') #give your '/train/' folder destination
+FOLDER = glob.glob('..../train/*_13*') #give your '/train/' folder destination
 random.shuffle(FOLDER)
 Ftrain = FOLDER[0:40]
 Fval = FOLDER[40:50]
