88de0a7b1eb47ea59c48adec2816a008fa54e354
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2020-10-26 18:57:14 +0200

    Update create_xys.py

--- create_xys.py
+++ create_xys.py
@@ -7,11 +7,11 @@ import pandas as pd
 import glob
 import random
 
-patch_s=32 #define the desired patch size
-step 16 #define the step that will be used to extract the patches
+patch_s = 32 #define the desired patch size
+step = 16 #define the step that will be used to extract the patches
 
-Ftrain = np.load('../Fsplit/Ftrain.npy').tolist()
-Fval = np.load('../Fsplit/Fval.npy').tolist()
+Ftrain = np.load('./Fsplit/Ftrain.npy').tolist()
+Fval = np.load('./Fsplit/Fval.npy').tolist()
 
 def shuffle(vector):
   vector = np.asarray(vector)
@@ -60,40 +60,6 @@ def sliding_window_train(i_city, labeled_areas, label, window_size, step):
     return np.asarray(city)
 
 
-def sliding_window_val(i_city, labeled_areas, label, window_size, step):
-    city=[]
-    fpatches_labels=[]
-
-    x=0
-    while (x!=label.shape[0]):
-     y=0
-     while(y!=label.shape[1]):
-
-               if (not y+window_size > label.shape[1]) and (not x+window_size > label.shape[0]):
-                line=np.array([x,y, labeled_areas.index(i_city), 0])
-                city.append(line)
-
-               stride = step
-
-               if y + window_size == label.shape[1]:
-                  break
-
-               if y + window_size > label.shape[1]:
-                y = label.shape[1] - window_size
-               else:
-                y = y+stride
-
-     if x + window_size == label.shape[0]:
-        break
-
-     if x + window_size > label.shape[0]:
-       x = label.shape[0] - window_size
-     else:
-      x = x+stride
-
-    return np.asarray(city)
-    
-
 if os.path.exists('xys'):
     shutil.rmtree('xys')
 os.mkdir('xys')
@@ -125,7 +91,7 @@ for i_city in Fval:
  path = i_city + '/change/change.tif'
  val_gt = io.imread(path)
 
- xy_city =  sliding_window_val(i_city, Fval, val_gt, patch_s, patch_s)
+ xy_city =  sliding_window_train(i_city, Fval, val_gt, patch_s, patch_s)
  cities.append(xy_city)
 
 final_cities = np.concatenate(cities, axis=0)
@@ -137,4 +103,3 @@ df = pd.DataFrame({'X': list(final_cities[:,0]),
                    'image_ID': list(final_cities[:,2]),
                    })
 df.to_csv('./xys/myxys_val.csv', index=False, columns=["X", "Y", "image_ID"])
-
