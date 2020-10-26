35f3e39926a4b58359d83f34d3b0039a9ce18eb5
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2020-10-26 19:08:50 +0200

    Update Fsplit.py

--- Fsplit.py
+++ Fsplit.py
@@ -2,13 +2,18 @@ import numpy as np
 import os
 import glob
 import random
+import shutil
 
-
-FOLDER = glob.glob('/home/mariapap/DATA/SPACENET7/train/*_13*')
+FOLDER = glob.glob('/home/mariapap/DATA/SPACENET7/train/*_13*') #give your '/train/' folder destination
 random.shuffle(FOLDER)
 Ftrain = FOLDER[0:40]
 Fval = FOLDER[40:50]
 Ftest = FOLDER[50:60]
+
+
+if os.path.exists('Fsplit'):
+    shutil.rmtree('Fsplit')
+os.mkdir('Fsplit')
 np.save('./Fsplit/Ftrain.npy', Ftrain)
 np.save('./Fsplit/Fval.npy', Fval)
 np.save('./Fsplit/Ftest.npy', Ftest)
