9f21b46b3563d2b1fc5331fc74f1db25568468fd
Maria Papadomanolaki <mar.papadomanolaki@gmail.com>
2020-10-27 14:23:53 +0200

    Update make_change.py

--- preprocess_train_images/make_change.py
+++ preprocess_train_images/make_change.py
@@ -4,24 +4,30 @@ import numpy as np
 import cv2
 import glob
 import shutil
+import argparse
 
-L = glob.glob('*_13*')
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_images_folder', type=str, default='./',
+                    help='downloaded folder of SpaceNet7 training images')
 
+args = parser.parse_args()
+
+L = glob.glob(args.train_images_folder + '*_13*')
 cnt = 0
 
 for c in range (0, len(L)):
     l = L[c]
     print(c ,'/' ,len(L), ' foders processed')
-    if os.path.exists('./{}/change'.format(l)):
-        shutil.rmtree('./{}/change'.format(l))
-    os.mkdir('./{}/change'.format(l))
+    if os.path.exists(args.train_images_folder + '{}/change'.format(l)):
+        shutil.rmtree(args.train_images_folder + '{}/change'.format(l))
+    os.mkdir(args.train_images_folder + '{}/change'.format(l))
 
 
-    b1 = io.imread('./{}/buildings/buildings1.tif'.format(l))
-    b2 = io.imread('./{}/buildings/buildings2.tif'.format(l))
+    b1 = io.imread(args.train_images_folder + '{}/buildings/buildings1.tif'.format(l))
+    b2 = io.imread(args.train_images_folder + '{}/buildings/buildings2.tif'.format(l))
 
 
     b2[np.where(b1==1)] = 0
-    cv2.imwrite('./' + l + '/change/change.tif', b2)
+    cv2.imwrite(args.train_images_folder + l + '/change/change.tif', b2)
 
     cnt = cnt + 1
